f2ba8d3c49f1149e7399b545cefdce840e717828
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2026-05-12 19:40:58 -0300

    feat: implement user management with create and find functionalities

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() body: CreateUserDto) {
+    return await this.usersService.create(body);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.usersService.findAll();
+  }
+}
